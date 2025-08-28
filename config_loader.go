3a83b6d09f90903ac5627df82a992acee527c11f
hansir-hsj <hanshijiex@163.com>
2025-08-28 10:48:47 +0800

    feat: Add support for loading task definitions from Json config files

--- config_loader.go
+++ config_loader.go
@@ -1,6 +1,7 @@
 package golitecron
 
 import (
+	"encoding/json"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -14,6 +15,20 @@ func LoadFromYaml(path string) (*Config, error) {
 
 	var config Config
 	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func LoadFromJson(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(data, &config); err != nil {
 		return nil, err
 	}
 
