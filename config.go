b66cb345b112217476d55c264eaf927903d67de4
hanshijie <hanshijie@hanshijiedeMacBook-Pro.local>
2025-08-28 10:48:47 +0800

    feat: Add support for loading task definitions from Json config files

--- config.go
+++ config.go
@@ -1,16 +1,16 @@
 package golitecron
 
 type TaskConfig struct {
-	ID            string `yaml:"id"`
-	CronExpr      string `yaml:"cron_expr"`
-	Timeout       int64  `yaml:"timeout"`
-	Retry         int    `yaml:"retry"`
-	Location      string `yaml:"location"`
-	EnableSeconds bool   `yaml:"enable_seconds"`
-	EnableYears   bool   `yaml:"enable_years"`
-	FuncName      string `yaml:"func_name"`
+	ID            string `yaml:"id" json:"id"`
+	CronExpr      string `yaml:"cron_expr" json:"cron_expr"`
+	Timeout       int64  `yaml:"timeout" json:"timeout"`
+	Retry         int    `yaml:"retry" json:"retry"`
+	Location      string `yaml:"location" json:"location"`
+	EnableSeconds bool   `yaml:"enable_seconds" json:"enable_seconds"`
+	EnableYears   bool   `yaml:"enable_years" json:"enable_years"`
+	FuncName      string `yaml:"func_name" json:"func_name"`
 }
 
 type Config struct {
-	Tasks []TaskConfig `yaml:"tasks"`
+	Tasks []TaskConfig `yaml:"tasks" json:"tasks"`
 }
